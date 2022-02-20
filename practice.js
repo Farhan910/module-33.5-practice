fc5c0a41ef6c5d906c94838001ab76294c9ce5aa
Farhan910 <farhanfaruk221@gmail.comm>
2022-02-21 01:40:00 +0600

    1st

---
+++ practice.js
@@ -0,0 +1,82 @@
+// let cars = 'toyota'
+// cars = cars
+
+// const myCars = 'bmw'
+
+
+// console.log(myCars);
+
+
+// const name = 'sadik'
+
+// const sadikName = `
+// hello ${name} welcome to our team
+// `
+
+// console.log(sadikName);
+
+
+// const products = {
+//     name:'tata', price:80000,color :'black',
+//     name:'BMW', price:60000,color :'blue',
+//     name:'toyota', price:50000,color :'green',
+//     name:'riksa', price:2000,color :'white',
+//     name:'car', price:3000,color :'red'
+// }
+
+// const newProduct = `
+// this is my car ${products.name,products.price} taka price
+// `
+// console.log(newProduct);
+
+
+// const sum = sum => sum * 5;
+// const value = sum(10)
+
+
+
+// const sum = (num1,num2)=> (num1,num2 + 2) * (num1,num2) ;
+// const num = sum (10,20)
+// console.log(num);
+
+// const doMath =
+
+//     (num1,num2) =>{
+
+//         const sum = num1 + 2;
+//         const num = num2 + 2;
+
+//         console.log(sum,num);
+//         const bum = sum * num;
+//         return bum
+        
+        
+//     }
+
+//     const total = doMath(21,10)
+//     console.log(total);
+
+
+// const numbers = (num1,num2,num3) => num1 * num2* num3;
+
+// const total = numbers(20,10,30)
+
+// console.log(total);
+
+
+// const numbers = [10,20,30,40,50,60,70]
+
+// const number  = numbers.map(number => number * 5) 
+// const total = [...number]
+// console.log(total);
+    
+    
+ 
+
+
+
+
+
+
+
+
